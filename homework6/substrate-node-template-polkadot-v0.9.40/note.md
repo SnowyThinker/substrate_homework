c139193ca1aef319d4525c96a990cef226fb85c7
SnowyThinker <wj.andrew@outlook.com>
2023-06-28 21:57:28 +0800

    homework6

---
+++ homework6/substrate-node-template-polkadot-v0.9.40/note.md
@@ -0,0 +1,10 @@
+
+
+~~~
+https://github.com/kaichaosun/play-substrate/blob/master/pallets/poe/src/tests.rs
+~~~
+
+* run benchmark
+~~~
+cargo build --release --features runtime-benchmarks
+~~~
--- homework6/substrate-node-template-polkadot-v0.9.40/pallets/poe/src/benchmarking.rs
+++ homework6/substrate-node-template-polkadot-v0.9.40/pallets/poe/src/benchmarking.rs
@@ -1,14 +1,10 @@
-//! Benchmarking setup for pallet-template
-
-use super::*;
-
-#[allow(unused)]
-use crate::Pallet as Template;
-use frame_benchmarking::v1::{benchmarks, whitelisted_caller};
+use crate::*;
+use frame_benchmarking::v1::{benchmarks, whitelisted_caller, account};
 use frame_system::RawOrigin;
+use sp_std::vec;
 
 fn assert_last_event<T: Config>(generic_event: <T as Config>::RuntimeEvent) {
-	frame_system::Pallet::<T>::assert_last_event(generic_event.into())
+	frame_system::Pallet::<T>::assert_last_event(generic_event.into());
 }
 
 benchmarks! {
@@ -16,7 +12,7 @@ benchmarks! {
 		let d in 0 .. T::MaxClaimLength::get();
 		let claim = BoundedVec::try_from(vec![0; d as usize]).unwrap();
 		let caller: T::AccountId = whitelisted_caller();
-	}; _(RawOrigin::Signed(caller.clone()), claim.clone())
+	}: _(RawOrigin::Signed(caller.clone()), claim.clone())
 	verify {
 		assert_last_event::<T>(Event::ClaimCreated(caller, claim).into())
 	}
